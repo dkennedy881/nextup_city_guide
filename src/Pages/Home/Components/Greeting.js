3477cc8882ef86aed3a6a6abfa0318963dfa42cf
DeMarcus Kennedy <demarcus@demarcuss-mbp.lan>
2020-08-14 14:50:25 -0500

    moving computers
    Signed-off-by: DeMarcus Kennedy <demarcus@demarcuss-mbp.lan>

--- src/Pages/Home/Components/Greeting.js
+++ src/Pages/Home/Components/Greeting.js
@@ -1,21 +1,25 @@
-import React from "react"
+import React from "react";
+
+import "../style.css";
 
 import { BarChartGraphic } from "./BarChartGraphic";
 
 const Greeting = () => {
-
-
-    return <div className="container-fluid" style={{ backgroundColor: "turquoise", height: "75vh", borderBottomStyle: "solid", borderBottomColor: "red" }}>
-        <div className="row" >
-            <div className="col-md-7" id="GreetingTextContainer">
-                <h2 id="GreetingTitle" style={{ color: "white", fontWeight: "800" }}>Become a Partner or Tour the City</h2>
-                <h4 id="GreetingSubTitle" style={{ color: "yellow", fontWeight: "300" }}>City Guide</h4>
-            </div>
-            <div className="col-md-5" style={{ display: "flex", justifyContent: "center" }}>
-                <BarChartGraphic />
-            </div>
+  return (
+    <div className="container-fluid" id="GreetingContainer">
+      <div className="row padded">
+        <div className="col-md-7" id="GreetingTextContainer">
+          <h2 id="GreetingTitle">
+            Become a Partner or <br /> Tour the City
+          </h2>
+          <h4 id="GreetingSubTitle">City Guide</h4>
         </div>
+        <div className="col-md-5" id="GraphContainer">
+          <BarChartGraphic />
+        </div>
+      </div>
     </div>
-}
+  );
+};
 
 export { Greeting };
