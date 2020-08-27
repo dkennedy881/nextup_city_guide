f3c531d9776813f7a8a1e38bc14e3d372eea829a
DeMarcus Kennedy <demarcus@demarcuss-air.lan>
2020-08-26 21:37:27 -0500

    will do deploy now
    Signed-off-by: DeMarcus Kennedy <demarcus@demarcuss-air.lan>

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -9,6 +9,11 @@ import { Home } from "./Pages";
 import { NavBar } from "./Components/NavBar/NavBar";
 
 function App() {
+  useEffect(() => {
+    fetch("/api/getList")
+      .then((res) => res.json())
+      .then((list) => console.log(list));
+  }, []);
   return (
     <div className="App">
       <NavBar />
