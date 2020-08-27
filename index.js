b614bc27f7f06ef9510d6e6a2ab6a3ebfdafa621
DeMarcus Kennedy <demarcus@demarcuss-air.lan>
2020-08-27 00:33:17 -0500

    single change to trigger deploy
    Signed-off-by: DeMarcus Kennedy <demarcus@demarcuss-air.lan>

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.static(__dirname + "/client/build"));
 //   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 // });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port);
 
 console.log("App is listening on port " + port);
