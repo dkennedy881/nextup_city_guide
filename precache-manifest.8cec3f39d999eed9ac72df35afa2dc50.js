5c81119e83dcc0fb04d95ab5a7cede86ebaf674c
DeMarcus Kennedy <demarcus@demarcuss-mbp.lan>
2020-09-04 00:42:42 -0500

    Updates

---
+++ precache-manifest.8cec3f39d999eed9ac72df35afa2dc50.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6232f1de1e729285de61823a0abfaae8",
+    "url": "/nextup_city_guide/index.html"
+  },
+  {
+    "revision": "76d449bc60febcea45d0",
+    "url": "/nextup_city_guide/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "975c04b60da2a7cbf0d7",
+    "url": "/nextup_city_guide/static/css/main.29f8665f.chunk.css"
+  },
+  {
+    "revision": "76d449bc60febcea45d0",
+    "url": "/nextup_city_guide/static/js/2.1c01901f.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/nextup_city_guide/static/js/2.1c01901f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "975c04b60da2a7cbf0d7",
+    "url": "/nextup_city_guide/static/js/main.477500e5.chunk.js"
+  },
+  {
+    "revision": "71c2bd0c187cbbbdb76d",
+    "url": "/nextup_city_guide/static/js/runtime-main.a3a73418.js"
+  },
+  {
+    "revision": "4d70629f7504b1126ee3a601d48b443a",
+    "url": "/nextup_city_guide/static/media/icon.4d70629f.jpeg"
+  },
+  {
+    "revision": "ce39437b218db0ec1e4100e44fb95f63",
+    "url": "/nextup_city_guide/static/media/next-up.ce39437b.png"
+  }
+]);
