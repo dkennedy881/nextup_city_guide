957b8ab99213f9cad44cc9b4442677482b79e567
DeMarcus Kennedy <demarcus@demarcuss-mbp.lan>
2020-09-04 22:26:38 -0500

    deploying to site
    Signed-off-by: DeMarcus Kennedy <demarcus@demarcuss-mbp.lan>

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nextup_city_guide/precache-manifest.fe25f37bcb4f099f39ccc60e79fc232d.js"
+  "/precache-manifest.eee82cf5c808a3a6a235e352b187d6c0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nextup_city_guide/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
