68f07517e406df7adc6960555e76c97746430f48
DeMarcus Kennedy <demarcus@DeMarcuss-MacBook-Pro.local>
2020-09-06 22:05:30 -0500

    trying to deploy
    Signed-off-by: DeMarcus Kennedy <demarcus@DeMarcuss-MacBook-Pro.local>

---
+++ client/build/precache-manifest.16705deb5aca5a05b9ff317ebaf032b4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7a9648fc3b330254d3b41e568f4602f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94aeb01965b3b161f391",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "cb11519bb9307b081fd5",
+    "url": "/static/css/main.31e45f70.chunk.css"
+  },
+  {
+    "revision": "94aeb01965b3b161f391",
+    "url": "/static/js/2.865876cd.chunk.js"
+  },
+  {
+    "revision": "c69f5d3c73fa519b737611ebd2097e4a",
+    "url": "/static/js/2.865876cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb11519bb9307b081fd5",
+    "url": "/static/js/main.a931392c.chunk.js"
+  },
+  {
+    "revision": "dfc61d2d76012f727be1",
+    "url": "/static/js/runtime-main.f4e8807e.js"
+  },
+  {
+    "revision": "4d70629f7504b1126ee3a601d48b443a",
+    "url": "/static/media/icon.4d70629f.jpeg"
+  },
+  {
+    "revision": "ce39437b218db0ec1e4100e44fb95f63",
+    "url": "/static/media/next-up.ce39437b.png"
+  }
+]);
